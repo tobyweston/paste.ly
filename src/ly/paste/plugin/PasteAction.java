a696fe0f1e51b8125390c039b2e2e78a4fa9cf1a
Toby Weston <toby.weston@gmail.com>
2014-04-06 19:56:47 +0100

    misc

--- src/ly/paste/plugin/PasteAction.java
+++ src/ly/paste/plugin/PasteAction.java
@@ -2,18 +2,17 @@ package ly.paste.plugin;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.ui.awt.RelativePoint;
 import ly.paste.robot.Clipboard;
 
-import javax.swing.*;
 import java.awt.*;
 
 import static com.intellij.openapi.actionSystem.CommonDataKeys.PROJECT;
 import static com.intellij.openapi.ui.MessageType.ERROR;
-import static com.intellij.openapi.ui.MessageType.INFO;
 import static com.intellij.openapi.ui.popup.Balloon.Position.atRight;
 
 public class PasteAction extends AnAction {
@@ -35,7 +34,7 @@ public class PasteAction extends AnAction {
     }
 
 	private void popup(final Exception e, final AnActionEvent event) {
-		SwingUtilities.invokeLater(new Runnable() {
+		ApplicationManager.getApplication().invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				StatusBar statusBar = WindowManager.getInstance().getStatusBar(PROJECT.getData(event.getDataContext()));
